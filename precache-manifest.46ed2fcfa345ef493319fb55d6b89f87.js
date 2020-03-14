7bae4d95b9c496ca41eab52f401e5c17df4f7461
Aravind Palani <tag.aravind@gmail.com>
2020-03-14 21:39:04 +0530

    Updates

---
+++ precache-manifest.46ed2fcfa345ef493319fb55d6b89f87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c2006ace2f4ea95a60eb5e07390ed2",
+    "url": "/React_LoginPage/index.html"
+  },
+  {
+    "revision": "b05d7bade4b1e6372215",
+    "url": "/React_LoginPage/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "daeb58a28312d9adedc4",
+    "url": "/React_LoginPage/static/css/main.6cd60d9c.chunk.css"
+  },
+  {
+    "revision": "b05d7bade4b1e6372215",
+    "url": "/React_LoginPage/static/js/2.a6f7ead2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_LoginPage/static/js/2.a6f7ead2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daeb58a28312d9adedc4",
+    "url": "/React_LoginPage/static/js/main.74e83f75.chunk.js"
+  },
+  {
+    "revision": "b6d23f397b25f8397b9e",
+    "url": "/React_LoginPage/static/js/runtime-main.93f501d9.js"
+  },
+  {
+    "revision": "12bbb9383f4fe373db239252832aebfa",
+    "url": "/React_LoginPage/static/media/Note_Paper.12bbb938.png"
+  }
+]);
